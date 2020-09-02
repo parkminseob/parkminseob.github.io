f0bddac03de241389b40e6f561fbcc7538fab982
parkminseob <minsub.prk@gmail.com>
2020-09-02 09:36:27 +0900

    finally

---
+++ assets/js/plugins/maker-widget.js
@@ -0,0 +1,14 @@
+<script>
+    (function(d, h, m){
+        var js, fjs = d.getElementsByTagName(h)[0];
+        if (d.getElementById(m)){return;}
+        js = d.createElement(h); js.id = m;
+        js.onload = function(){
+            window.makerWidgetComInit({
+            position: "left",          
+            widget: "gecvihtgknl8seoc-yvoonrhawq9vv9mr-pgo3a0wmuxpimmwi"                
+        })};
+        js.src = "https://makerwidget.com/js/embed.js";
+        fjs.parentNode.insertBefore(js, fjs)
+    }(document, "script", "dhm"))
+</script>
